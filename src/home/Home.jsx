a4a4b0b599085d01278005a97650d9636eae6a1e
Carlos Alberto Vivanco Rodriguez <sistemas.cavr@gmail.com>
2025-05-06 20:45:46 -0500

    Se arreglo la variable de entorno VITE_API_BASE_URL para por der acceder correctamente a las imagenes de frank y arboldelavida

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,14 +1,20 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './home.css';
+
 export default function Home() {
-    
-    return (    
-        <div className="cuerpoConImagenFondo">
-            <h1>
-                <div>Frank Barmak Raich</div>
-                <div>My Life</div>
-            </h1>
-        </div>
-    );
+  const [apiUrl, setApiUrl] = useState('');
+
+  useEffect(() => {
+    const apiUrl = import.meta.env.VITE_API_BASE_URL;
+    setApiUrl(apiUrl);
+  }, []);
+
+  return (
+    <div className="cuerpoConImagenFondo" style={{ backgroundImage: `url(${apiUrl}/media/fotos/arboldelavida.png)` }}>
+      <h1>
+        <div>Frank Barmak Raich</div>
+        <div>My Life</div>
+      </h1>
+    </div>
+  );
 }
