6f04df5518efd516f1b9e4143d410303049bb927
Carlos Alberto Vivanco Rodriguez <sistemas.cavr@gmail.com>
2025-05-06 21:07:51 -0500

    correccion .env

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'VITE_API_BASE_URL': process.env.VITE_API_BASE_URL,
-  },
 });
